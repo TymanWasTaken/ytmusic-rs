f920c926913f526e5a2d3997c0232cfbb62e50f4
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-04-02 12:29:03 -0600

    fmt and fix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Button};
+use sha1::{Digest, Sha1};
 use std::fs;
 use std::time::{SystemTime, UNIX_EPOCH};
-use sha1::{Sha1, Digest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -39,14 +39,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 }
 
 fn get_authorization(headers: &String) -> String {
-    let cookie_line = headers.split("\n").find(|&x| x.starts_with("Cookie: ")).expect("Fuck v2");
-    let sapisid = cookie_line.split(";").find(|&x| x.contains("__Secure-1PSID=")).expect("Fuck v3").trim().split("=").nth(1).expect("Fuck v4");
+    let cookie_line = headers
+        .split("\n")
+        .find(|&x| x.starts_with("Cookie: "))
+        .expect("Fuck v2");
+    let sapisid = cookie_line
+        .split(";")
+        .find(|&x| x.contains("__Secure-1PSID="))
+        .expect("Fuck v3")
+        .trim()
+        .split("=")
+        .nth(1)
+        .expect("Fuck v4");
     let since_the_epoch = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("Fuck v5")
         .as_secs();
     let mut hasher = Sha1::new();
-    hasher.update(format!("{} {} {}", sapisid, "https://music.youtube.com", since_the_epoch).as_bytes());
+    hasher.update(
+        format!(
+            "{} {} {}",
+            since_the_epoch, sapisid, "https://music.youtube.com"
+        )
+        .as_bytes(),
+    );
     let hash = hasher.finalize();
     let hash = hash.as_slice().to_vec();
     let hash = hex::encode(&hash);
