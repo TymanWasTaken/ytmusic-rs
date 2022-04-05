512674a41a592a06258af63de0c6a56a3ed2375b
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-04-05 17:25:51 -0600

    e

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,36 @@ mod ui;
 
 use gtk::prelude::*;
 use gtk::Application;
-// use reqwest::Method;
-// use ytmusic_api::structs::*;
-// use ytmusic_api::YtMusicClient;
+use reqwest::Method;
+use ytmusic_api::structs::*;
+use ytmusic_api::YtMusicClient;
+use ytmusic_api::parsing::{YtMusicResponse, ResponseType};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // let client = YtMusicClient::new("headers.txt");
-    // client.set_headers().await;
+    let client = YtMusicClient::new("headers.txt");
+    client.set_headers().await;
 
-    // let result = client
-    //     .endpoint("browse")
-    //     .make_request(
-    //         Method::POST,
-    //         RequestBody {
-    //             browseId: "FEmusic_liked_playlists".to_string(),
-    //             context: RequestContext::new(),
-    //         }
-    //         .as_body(),
-    //     )
-    //     .await
-    //     .unwrap();
+    let result = client
+        .endpoint("browse")
+        .make_request(
+            Method::POST,
+            RequestBody {
+                browseId: "FEmusic_liked_playlists".to_string(),
+                context: RequestContext::new(),
+            }
+            .as_body(),
+        )
+        .await
+        .unwrap()
+        .parse(ResponseType::LibraryPlaylists)
+        .await;
 
     let application = Application::builder()
         .application_id("tech.tyman.YtMusicRs")
         .build();
 
-    application.connect_activate(ui::setup_ui);
+    application.connect_activate(|app| ui::setup_ui(app, result));
 
     application.run();
 
